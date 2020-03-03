ea1ba1bd26660eac9600e5ea2c0c8601526eea49
Wanlin-Lu <luwanlin@126.com>
2020-03-03 11:15:16 +0800

    shared: navigation navs

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import MainHeader from './MainHeader'
+import NavLinks from './NavLinks'
+import sideDrawer from './SideDrawer'
+import './MainNavigation.css'
+import SideDrawer from './SideDrawer'
+
+const MainNavigation = props => {
+  return (
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+}
+
+export default MainNavigation
