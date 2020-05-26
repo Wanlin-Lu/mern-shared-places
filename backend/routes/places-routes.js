b5b36ddb07313591dfb986d0fa27540d8f8a785d
Wanlin-Lu <luwanlin@126.com>
2020-05-26 16:16:34 +0800

    Place Routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,49 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log("Get  Request in Places");
-  res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lng: 116.410829,
+      lat: 39.881913,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lng: 116.410829,
+      lat: 39.881913,
+    },
+    creator: "u2",
+  },
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  })
+  res.json({ place }); // => { place } => { place: place }
+})
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+  res.json({ place })
 })
 
 module.exports = router;
