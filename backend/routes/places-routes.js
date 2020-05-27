67db1b944eabbfe3f980dece23340ed17adc126c
Wanlin-Lu <luwanlin@126.com>
2020-05-27 11:59:06 +0800

    Adding Controllers

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,64 +1,11 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placeControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lng: 116.410829,
-      lat: 39.881913,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Emp. State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lng: 116.410829,
-      lat: 39.881913,
-    },
-    creator: "u2",
-  },
-];
+router.get('/:pid', placeControllers.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  })
-
-  if (!place) {
-   throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-
-  res.json({ place }); // => { place } => { place: place }
-})
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-
-  res.json({ place })
-})
+router.get('/user/:uid', placeControllers.getPlaceByUserId);
 
 module.exports = router;
