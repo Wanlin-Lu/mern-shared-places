fa84c43f74b33744a2b0911bdfdbb7c2aff0cf0c
Wanlin-Lu <luwanlin@126.com>
2020-05-30 13:17:56 +0800

    debug

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer')
 const uuid = require('uuid/v1')
 
-const MINE_TYPE_MAP = {
+const MIME_TYPE_MAP = {
   "image/png": "png",
   "image/jpeg": "jpeg",
   "image/jpg": "jpg"
@@ -13,12 +13,12 @@ const fileUpload = multer({
       cb(null, 'uploads/images')
     },
     filename: (req, file, cb) => {
-      const ext = MINE_TYPE_MAP[file.minetype];
+      const ext = MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid() + "." + ext);
     }
   }),
   fileFilter: (req, file, cb) => {
-    const isValid = !!MINE_TYPE_MAP[file.mimetype];
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
     let error = isValid ? null : new Error("Invalid minetype");
     cb(error, isValid)
   }
